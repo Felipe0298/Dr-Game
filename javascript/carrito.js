beff9e320db82b486a4245e8d1f68620a851aef0
Felipe Hoyos <felipe02-98@hotmail.com>
2022-10-31 18:57:43 -0500

    Btn-vaciar

--- javascript/carrito.js
+++ javascript/carrito.js
@@ -15,19 +15,10 @@ function actualizarCarrito(){
             <td><button class="btn btn-primary" id="btn eliminar__item${juegosCompradosFinal.id}"><i class="bi bi-x-circle-fill"></i></button></td>
         </tr>`;
     
-        
     }
 }
 
 actualizarCarrito();
-
-function eliminarJuego (){
-    let f = arrayXbox.indexOf(`eliminar__item${juegosCompradosFinal.id}`);
-    console.log(f);
-        
-}
-
-eliminarJuego();
 
 
 let compraTotal = arrayXbox.reduce((acumulador,juegoComprado) => acumulador + juegoComprado.price,0);
@@ -35,5 +26,42 @@ let compraTotal = arrayXbox.reduce((acumulador,juegoComprado) => acumulador + ju
 let compraTotalFinal = document.getElementById("total");
 compraTotalFinal.innerHTML = "Total a pagar $: "+compraTotal;
 
+const btnVaciar = document.getElementById("vaciar-carrito");
+    
+btnVaciar.addEventListener("click",() =>{
+        juegosCompradosFinal = [];
+        localStorage.clear();
+        /* actualizarCarrito(); */
+
+        document.getElementById("tablajuegos").innerHTML = "";
+        compraTotal=0;
+        document.getElementById("total");
+        compraTotalFinal.innerHTML = "Total a pagar $: "+compraTotal;
+        
+    })
+
+/* document.getElementById("eliminar__item6").addEventListener("click",console.log("click")
+); */
+
+/* function eliminarJuego ()  {
+    const index = arrayXbox.findIndex(object => {
+        return object.id ===arrayXbox.name;
+    });
+
+    juegosCompradosFinal.forEach(juegoAEliminar => {
+        document.getElementById(`eliminar__item${juegosCompradosFinal.id}`).addEventListener("click",function(){
+            removerItem(juegoAEliminar);
+        })
+    });
+
+    console.log(index);
+    
+}
+
+eliminarJuego();
+
+function removerItem (){
+
+} */
 
 
